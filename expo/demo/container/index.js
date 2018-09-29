5e6e28a015b8ed49887e206572807c2704f7cd5a
huangciyin <ciyinhuang@gmail.com>
2018-09-29 18:22:08 +0800

    up

--- expo/demo/container/index.js
+++ expo/demo/container/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
-import {Button} from 'popui-rn';
+// import {Button} from 'popui-rn';
+// import Button from '../../../components/button/index.native';
 
 export default class App extends React.Component {
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <Text>Common Container</Text>
-        <Button>Start</Button>
+        {/* <Button>Start</Button> */}
       </SafeAreaView>
     );
   }
