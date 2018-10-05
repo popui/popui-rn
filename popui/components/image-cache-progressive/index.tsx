a180b9d75f93c0620ce06e8a2eb09efed5f79fd1
ciyinhuang <ciyinhuang@gmail.com>
2018-10-06 01:06:53 +0800

    split comp

---
+++ popui/components/image-cache-progressive/index.tsx
@@ -0,0 +1,9 @@
+import Image from './Image'
+import CacheManager from './CacheManager'
+import ImageBackground from './ImageBackground'
+
+export default {
+  Image,
+  CacheManager,
+  ImageBackground
+}
