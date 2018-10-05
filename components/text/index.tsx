f5d72159a6cdfaf17cd89ff6d77e5ab20fc24812
ciyinhuang <ciyinhuang@gmail.com>
2018-10-06 01:11:28 +0800

    merge comp

--- components/text/index.tsx
+++ components/text/index.tsx
@@ -1,1 +1,21 @@
-export { Text as default } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text as RNText } from 'react-native'
+import V from '../style/themes/weui'
+
+export interface ITextProps {
+  style?: any
+  children?: any
+}
+const Text = ({ style, children, ...others }: ITextProps) => {
+  const styleObj: any = StyleSheet.flatten(style)
+  const fontSize = styleObj.fontSize || V.baseFontSize
+  const lineHeight = styleObj.lineHeight || fontSize * V.baseLineHeight
+
+  return (
+    <RNText style={[style, { fontSize, lineHeight }]} {...others}>
+      {children}
+    </RNText>
+  )
+}
+
+export default Text
--- components/view/index.tsx
+++ components/view/index.tsx
@@ -1,1 +1,16 @@
-export { View as default } from 'react-native';
+import PropTypes from 'prop-types'
+import React from 'react'
+import { View as RNView, ViewPropTypes } from 'react-native'
+
+const View = ({ style, children, ...others }) => {
+    return (
+        <RNView style={style} {...others}>{children}</RNView>
+    )
+}
+
+View.propTypes = {
+    style: ViewPropTypes.style,
+    children: PropTypes.node,
+}
+
+export default View
