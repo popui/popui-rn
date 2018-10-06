08e29504f2251dd798f571994323f69e1ae0dd87
ciyinhuang <ciyinhuang@gmail.com>
2018-10-06 23:14:37 +0800

    up

--- components/style/themes/weui/components/Msg.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    MsgPaddingTop: 36,
-    MsgIconGap: 30,
-    MsgTitleGap: 5,
-    MsgTextGap: 25,
-    MsgOprGap: 25,
-    MsgExtraAreaGap: 15,
-    MsgExtraAreaOfMinHeight: 438,
-}
--- components/style/themes/weui/index.ts
+++ components/style/themes/weui/index.ts
@@ -1,7 +1,6 @@
 import merge from 'lodash/merge'
 import _button from './components/Button'
 import _cell from './components/Cell'
-import _msg from './components/Msg'
 import _progress from './components/Progress'
 import _dialog from './components/Dialog'
 import _form from './components/Form'
@@ -12,7 +11,6 @@ const defaultConfig = merge(
   _global,
   _button,
   _cell,
-  _msg,
   _progress,
   _dialog,
   _form
