4430a3f76e28ccf94d7f9970521762b99939a2d7
ciyinhuang <ciyinhuang@gmail.com>
2018-10-06 20:40:36 +0800

    up

--- components/grid/demo/basic.tsx
+++ components/grid/demo/basic.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
-import { Grid } from 'popui-rn';
+import React from "react";
+import { ScrollView, Text, View } from "react-native";
+import { Grid, WhiteSpace } from "popui-rn";
 
 const data = Array.from(new Array(9)).map((_val, i) => ({
-  icon: 'https://weui.io/images/icon_tabbar.png',
-  text: `Item ${i}`,
+  icon: "https://weui.io/images/icon_tabbar.png",
+  text: `Item ${i}`
 }));
 
 export default class BasicGridExample extends React.Component<any, any> {
   render() {
     return (
       <ScrollView>
-        <View style={[{ margin: 10 }]}>
+        <View style={[{ padding: 10, backgroundColor: "#eee" }]}>
           <Text>Simple</Text>
         </View>
-          <Grid data={data} hasLine={false} 
-          />
-        <View style={[{ margin: 10 }]}>
+        <Grid data={data} hasLine={false} />
+        <WhiteSpace size="xl" />
+
+        <View style={[{ padding: 10, backgroundColor: "#eee" }]}>
+          <Text>Custom GridCell Style</Text>
+        </View>
+        <Grid
+          data={data}
+          columnNum={3}
+          containerStyle={{ backgroundColor: "#fbf9fe" }}
+          itemStyle={{ height: 150 }}
+        />
+        <WhiteSpace size="xl" />
+
+        <View style={[{ padding: 10, backgroundColor: "#eee" }]}>
           <Text>Carousel</Text>
         </View>
         <Grid
@@ -25,15 +37,7 @@ export default class BasicGridExample extends React.Component<any, any> {
           isCarousel
           onClick={(_el: any, index: any) => alert(index)}
         />
-        <View style={[{ margin: 10 }]}>
-          <Text>Custom GridCell Style</Text>
-        </View>
-        <Grid
-          data={data}
-          columnNum={3}
-          containerStyle={{backgroundColor: '#fbf9fe'}}
-          itemStyle={{ height: 150 }}
-        />
+        <WhiteSpace size="xl" />
       </ScrollView>
     );
   }
