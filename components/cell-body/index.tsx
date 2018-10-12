8a8bafe8b30761de98e61eb24e9fe434cd917ded
ciyinhuang <ciyinhuang@gmail.com>
2018-10-12 21:48:00 +0800

    use class

--- components/cell-body/index.tsx
+++ components/cell-body/index.tsx
@@ -1,50 +1,55 @@
-import PropTypes from 'prop-types'
-import React from 'react'
-import { StyleSheet, View, ViewPropTypes } from 'react-native'
+import  React from 'react'
+import { StyleSheet, View, ViewStyle } from 'react-native'
 import IconWeui from '../icon-weui'
 import V from '../style/themes/weui'
 import CellText from '../cell-text'
 
 const styles = StyleSheet.create({
-    cellBody: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-    },
-    error: {
-        flex: 1,
-        color: V.ColorWarn,
-    },
+  cellBody: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  error: {
+    flex: 1,
+    color: V.ColorWarn,
+  },
 })
 
-const CellBody = ({ error, children, style, ...others }) => {
-    const childrenWithProps = React.Children.map(children, child => {
-        if (typeof child === 'string') {
-            return <CellText style={[error ? styles.error : null]}>{child}</CellText>
-        }
-        if(typeof child === "string"){
-            return React.cloneElement(child, {
-                style: [child.props.style,
-                    error ? styles.error : null,
-                ],
-            })
-        }
-        return child
+
+export interface CellBodyProps {
+  error?: boolean;
+  children?: React.ReactNode;
+  style?: ViewStyle;
+}
+
+export interface CellBodyState {
+}
+
+export default class CellBodyComponent extends React.Component<CellBodyProps, CellBodyState> {
+
+  private renderChildrenWithProps = () => {
+    const { error, children } = this.props;
+    const childrenWithProps = React.Children.map(children, (child:any) => {
+      if (typeof child === 'string') {
+        const textStyles = error ? styles.error : null
+        return <CellText style={textStyles}>{child}</CellText>
+      }
+      return React.cloneElement(child, {
+        error
+      })
     })
-
+    return childrenWithProps
+  }
+  public render() {
+    const { error, style } = this.props;
     return (
-        <View style={[styles.cellBody, style, error ? { flexDirection: 'row' } : null]} {...others}>
-            {childrenWithProps}
-            {error ? <IconWeui name="warn"/> : false}
-        </View>
+      <View style={[styles.cellBody, style, error ? { flexDirection: 'row' } : null]} {...others}>
+        {this.renderChildrenWithProps()}
+        {error ? <IconWeui name="warn" /> : false}
+      </View>
     )
+  }
 }
 
-CellBody.propTypes = {
-    error: PropTypes.bool,
-    children: PropTypes.node,
-    style: ViewPropTypes.style,
-}
-
-export default CellBody
--- components/cell/index.tsx
+++ components/cell/index.tsx
@@ -1,6 +1,5 @@
-import PropTypes from 'prop-types'
 import React from 'react'
-import { StyleSheet, View, ViewPropTypes } from 'react-native'
+import { StyleSheet, View, ViewStyle } from 'react-native'
 import V from '../style/themes/weui'
 import TouchableWithFallback from '../touchable-with-fallback'
 
@@ -28,40 +27,46 @@ const styles = StyleSheet.create({
     },
 })
 
-const Cell = ({ access, vcode, error, first, disabled, children, style, ...others }) => {
-    const childrenWithProps = React.Children.map(children, child => {
-        if (access && child.type.name === 'CellFooter') {
-            return React.cloneElement(child, { access: true })
-        }
-        if (error && (child.type.name === 'CellHeader' || child.type.name === 'CellBody')) {
-            return React.cloneElement(child, { error: true })
-        }
-        return child
-    })
-
-    return (
-        <TouchableWithFallback underlayColor={V.BgColorActive} {...others} >
-            <View
-                style={[
-                    styles.cell,
-                    style,
-                    first ? styles.firstCell : null,
-                    vcode ? styles.vcodeCell : null,
-                    disabled ? styles.disabledCell : null,
-                ]}
-            >{childrenWithProps}</View>
-        </TouchableWithFallback>
-    )
+export interface CellProps {
+  first?: boolean,
+  access?: boolean,
+  vcode?: boolean,
+  error?: boolean,
+  disabled?: boolean,
+  children?: React.ReactNode,
+  style?: ViewStyle
 }
 
-Cell.propTypes = {
-    first: PropTypes.bool,
-    access: PropTypes.bool,
-    vcode: PropTypes.bool,
-    error: PropTypes.bool,
-    disabled: PropTypes.bool,
-    children: PropTypes.node,
-    style: ViewPropTypes.style,
+export interface CellState {
 }
 
-export default Cell
+export default class CellComponent extends React.Component<CellProps, CellState> {
+
+  public render() {
+    const { access, vcode, error, first, disabled, children, style, ...others } = this.props
+    const childrenWithProps = React.Children.map(children, (child:any) => {
+      if (access && child.type.name === 'CellFooter') {
+          return React.cloneElement(child, { access: true })
+      }
+      if (error && (child.type.name === 'CellHeader' || child.type.name === 'CellBody')) {
+          return React.cloneElement(child, { error: true })
+      }
+      return child
+  })
+
+  return (
+      <TouchableWithFallback underlayColor={V.BgColorActive} {...others} >
+          <View
+              style={[
+                  styles.cell,
+                  style,
+                  first ? styles.firstCell : null,
+                  vcode ? styles.vcodeCell : null,
+                  disabled ? styles.disabledCell : null,
+              ]}
+          >{childrenWithProps}</View>
+      </TouchableWithFallback>
+  )
+  }
+}
+
