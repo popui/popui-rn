871c44498a1af296ff6274ad586034b269e08303
ciyinhuang <ciyinhuang@gmail.com>
2018-10-17 00:29:26 +0800

    up

--- components/style/themes/weui/components/Dialog.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    DialogBackgroundColor: '#FFFFFF',
-    DialogLineColor: '#D5D5D6',
-    DialogLinkColor: '#3CC51F',
-    DialogLinkActiveBc: '#EEEEEE',
-    DialogGapWidth: 1.6 * 16,
-}
--- components/style/themes/weui/components/Progress.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    ProgressBg: '#EBEBEB',
-    ProgressColor: '#09BB07',
-    ProgressHeight: 3,
-    ProgressCloseBg: '#EF4F4F',
-    ProgressActiveBg: '#C13E3E',
-}
