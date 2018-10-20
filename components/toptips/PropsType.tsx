0ca3306e15f8031a3c2770a86e3805e7598a003f
ciyinhuang <ciyinhuang@gmail.com>
2018-10-20 19:26:30 +0800

    up

---
+++ components/toptips/PropsType.tsx
@@ -0,0 +1,12 @@
+import React from 'React';
+import {ViewStyle,TextStyle} from 'react-native';
+export interface ToptipsPropsType {
+  type?: 'primary' | 'warn' | 'info'
+  visible?: boolean
+  onShow?: Function
+  onClose?: Function
+  style?: ViewStyle
+  textStyle?: TextStyle
+  children?: React.ReactNode
+}
+
