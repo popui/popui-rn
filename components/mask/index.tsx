94d93fdaac737807a026bb9beb1238e99667f339
ciyinhuang <ciyinhuang@gmail.com>
2018-10-20 20:14:01 +0800

    up

--- components/mask/index.tsx
+++ components/mask/index.tsx
@@ -1,5 +1,20 @@
-import Mask from './Mask'
+import React from 'react'
+import { StyleSheet, TouchableWithoutFeedback, View, ViewPropTypes } from 'react-native'
+import { MaskPropsType } from "./PropsType";
+const styles = StyleSheet.create({
+    mask: {
+        flex: 1,
+        zIndex: 1000,
+    },
+})
 
-export default {
-    Mask,
-}
+const Mask = ({ transparent = false, style, onPress, children }:MaskPropsType) =>
+    <TouchableWithoutFeedback onPress={onPress}>
+        <View
+            style={[styles.mask, { backgroundColor: transparent ? 'transparent' : 'rgba(0,0,0,.6)' }, style]}
+        >
+            <TouchableWithoutFeedback>{children}</TouchableWithoutFeedback>
+        </View>
+    </TouchableWithoutFeedback>
+
+export default Mask
