22d3b21aa784f410e6c8e6bd5348c2016b59aa11
ciyinhuang <ciyinhuang@gmail.com>
2018-10-21 14:19:09 +0800

    up

--- components/theme-store/index.tsx
+++ components/theme-store/index.tsx
@@ -1,28 +1,44 @@
-import { action, observable } from 'mobx'
-import { persist } from 'mobx-persist'
+import { action, observable,runInAction } from "mobx";
+import { persist } from "mobx-persist";
+import defaultThemeVariables from "../style/themes/default";
 
 // debug
-import { createDebug } from '../_util/debug'
-const debug = createDebug('ig:store:IGUserStateStore')
+import { createDebug } from "../_util/debug";
+const debug = createDebug("ig:store:IGUserStateStore");
 
 class ThemeStore {
   @persist
   @observable
-  locale = ''
+  theme: string;
 
   @persist
   @observable
-  localeVars = {}
+  themeVars: Object;
+
+  constructor({
+    initialTheme,
+    initialThemeVars
+  }: {
+    initialTheme: string;
+    initialThemeVars: Object;
+  }) {
+    runInAction(()=>{
+      this.theme = initialTheme;
+      this.themeVars = initialThemeVars;
+    })
+  }
 
   @action.bound
-  setLocale(val: string) {
-    this.locale = val
+  setTheme(val: string) {
+    this.theme = val;
   }
   @action.bound
-  setLocaleVars(val: Object) {
-    this.localeVars = val
+  setThemeVars(val: Object) {
+    this.themeVars = val;
   }
 }
-export const themeStore = new ThemeStore()
-
-export default ThemeStore
+export const defaultThemeStore = new ThemeStore({
+  initialTheme: "default",
+  initialThemeVars: defaultThemeVariables
+});
+export default ThemeStore;
