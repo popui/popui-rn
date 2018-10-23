2e6cb512d13eb6bbd8145561cfc7976fd46f4312
huangciyin <ciyinhuang@gmail.com>
2018-10-23 09:23:55 +0800

    use tsx transformer

--- expo/demo/container/index.js
+++ expo/demo/container/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
-// import ActionSheetExample from 'popui-rn/components/action-sheet/demo/basic';
+import React from "react";
+import { StyleSheet, SafeAreaView, ScrollView } from "react-native";
+// import ProgressExample from 'popui-rn/components/progress/demo/basic';
+// import PickerExample from 'popui-rn/components/picker/demo/basic';
+// import BadgeExample from "popui-rn/components/badge/demo/basic";
+// import CellBadgeExample from "popui-rn/components/cell-badge/demo/basic";
+import ModalExample from "popui-rn/components/modal/demo/basic";
+// import Modal2Example from "popui-rn/components/modal2/demo/basic";
+// import Picker2Example from 'popui-rn/components/picker2/demo';
 // import { ActionSheet2 } from 'popui-rn'
-import AllComp from 'popui-rn/rn-kitchen-sink/allComponents';
-import ResourceLoader from '../common/ResourceLoader';
+// import AllComp from 'popui-rn/rn-kitchen-sink/allComponents';
+import ResourceLoader from "../common/ResourceLoader";
 export default class App extends React.Component {
     render() {
         return (<ResourceLoader>
         <SafeAreaView style={styles.container}>
-          <AllComp />
+          <ScrollView>
+            
+            
+            
+            
+            <ModalExample />
+            
+          </ScrollView>
         </SafeAreaView>
       </ResourceLoader>);
     }
@@ -16,6 +29,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#000',
     },
 });
