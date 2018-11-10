f81209eabf002db172513122cb9e573523bd01ad
ciyinhuang <ciyinhuang@gmail.com>
2018-11-10 21:35:06 +0800

    add comp

---
+++ components/material-text-input/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TextField } from 'react-native-material-textfield'
+
+class MaterialTextInput extends React.Component {
+  render() {
+    return <TextField {...this.props} />
+  }
+}
+
+export default MaterialTextInput
